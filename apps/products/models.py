1d9adefaa9470150b1ffef5494d70e00e5eec193
Timothy Kamau <teamorekamau97@gmail.com>
2020-10-11 20:45:55 +0300

    Image upload - change the upload image functionality to allow multiple file upload - create new Image model - add the respective image serializer - allow images to be uploaded automatically when creating a product

--- apps/products/models.py
+++ apps/products/models.py
@@ -11,7 +11,6 @@ class Category(MyBaseModel):
         return self.name
     
 
-
 class Product(MyBaseModel):
     name = models.CharField(max_length=60)
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -20,9 +19,13 @@ class Product(MyBaseModel):
     size = models.FloatField(null=True)
     price = models.FloatField(default=0)
     no_of_items = models.IntegerField(default=0)
+    product_images = models.CharField(max_length=50, null=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, blank=True, null=True)
-    product_images = models.ImageField(upload_to='images', blank=True)
 
     def __str__(self):
         return self.name
+
+class Image(MyBaseModel):
+    image = models.ImageField(upload_to="images", blank=True, null=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True)
     
